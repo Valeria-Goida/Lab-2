7b50fe9e060a1c23407d9cb5f73ca4ff6b27ca04
Valeria-Goida <goida.valeria@gmail.com>
2024-11-18 10:54:28 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.regex.*;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Введите строку с MAC-кодами.");
+     String macAddress = sc.nextLine();
+     String macAddressPattern = "([0-9a-fA-F]{2}[:]){5}([0-9a-fA-F]{2})";
+     Pattern p = Pattern.compile(macAddressPattern);
+     Matcher m = p.matcher(macAddress);
+     while(m.find()){
+        System.out.println("MAC-адресс: " + m.group());
+        }
+    }
+}
