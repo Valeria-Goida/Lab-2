9615d4b544117fd255e00033592fc785a895e6f6
Valeria-Goida <goida.valeria@gmail.com>
2024-11-19 12:15:40 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
-     System.out.println("Введите строку с MAC-кодами.");
+     System.out.println("Введите строку с MAC-адрессами");
      String macAddress = sc.nextLine();
      String macAddressPattern = "([0-9a-fA-F]{2}[:]){5}([0-9a-fA-F]{2})";
      Pattern p = Pattern.compile(macAddressPattern);
